bff8d5b8cd238fdaef23dcec3cef322ac2e2c9a4
GeneralSkywalker1 <60532178+GeneralSkywalker1@users.noreply.github.com>
2020-02-07 11:39:28 +0800

    Add files via upload

---
+++ 字符串替换.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[81];
+    gets(str);
+    for(int i = 0;i < strlen(str);i++)
+    {
+        if(str[i]>='A'&&str[i]<='Z')
+        {
+            str[i] = 'A'+'Z'-str[i];
+        }
+    }
+    puts(str);
+}
